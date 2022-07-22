de7d65aa972de9feab2545f283927eedd77c273e
Dev <devp2101@gmail.com>
2022-07-22 16:43:50 +0530

    query added in this migration file

--- console/migrations/m220722_110953_add_fields_in_user_table.php
+++ console/migrations/m220722_110953_add_fields_in_user_table.php
@@ -12,7 +12,11 @@ class m220722_110953_add_fields_in_user_table extends Migration
      */
     public function safeUp()
     {
-
+        $this->addColumn('{{%user}}','first_name',$this->string(125));
+        $this->addColumn('{{%user}}','last_name',$this->string(125));
+        $this->addColumn('{{%user}}','gender',$this->tinyInteger(1));
+        $this->addColumn('{{%user}}','age',$this->tinyInteger(4));
+        $this->addColumn('{{%user}}','profile_image',$this->string(255));
     }
 
     /**
