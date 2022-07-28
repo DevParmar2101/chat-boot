0c1ce58ae47435f1ee172d78ebc4d5a95983a890
DevParmar2101 <devp2101@gmail.com>
2022-07-28 19:05:15 +0530

    query is added in this migration file

--- console/migrations/m220728_131533_create_studying_university_name_table.php
+++ console/migrations/m220728_131533_create_studying_university_name_table.php
@@ -14,6 +14,11 @@ class m220728_131533_create_studying_university_name_table extends Migration
     {
         $this->createTable('{{%studying_university_name}}', [
             'id' => $this->primaryKey(),
+            'type_id' => $this->integer(11),
+            'university_name' => $this->string(255)->defaultValue(null),
+            'status' => $this->tinyInteger(2),
+            'user_id' => $this->integer(11),
+            'created_at' => $this->timestamp()
         ]);
     }
 
