68d227972135af73558d82c010c177ba8611dbbc
DevParmar2101 <devp2101@gmail.com>
2022-07-28 18:48:04 +0530

    migration file for studying_university_name tale is created and data also added in this file

---
+++ console/migrations/m220728_131533_create_studing_univercity_name_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%studying_university_name}}`.
+ */
+class m220728_131533_create_studing_univercity_name_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%studying_university_name}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%studying_university_name}}');
+    }
+}
