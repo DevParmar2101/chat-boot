e999082f6476885ea70e7f1f450eb0c85ef016b1
DevParmar2101 <devp2101@gmail.com>
2022-07-28 19:11:14 +0530

    query is added in branch_name file

--- console/migrations/m220728_133822_create_studying_branch_name_table.php
+++ console/migrations/m220728_133822_create_studying_branch_name_table.php
@@ -14,6 +14,11 @@ class m220728_133822_create_studying_branch_name_table extends Migration
     {
         $this->createTable('{{%studying_branch_name}}', [
             'id' => $this->primaryKey(),
+            'field_id' => $this->integer(11),
+            'branch_name' => $this->string(255),
+            'user_id' => $this->integer(11),
+            'status' => $this->tinyInteger(11),
+            'created_at' => $this->timestamp(),
         ]);
     }
 
