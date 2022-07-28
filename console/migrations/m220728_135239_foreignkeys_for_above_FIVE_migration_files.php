5ccf1f63e4286889f40f78e62919ebd2cac30d8c
DevParmar2101 <devp2101@gmail.com>
2022-07-28 19:30:26 +0530

    foreign_key query is added in this file

--- console/migrations/m220728_135239_foreignkeys_for_above_FIVE_migration_files.php
+++ console/migrations/m220728_135239_foreignkeys_for_above_FIVE_migration_files.php
@@ -12,7 +12,21 @@ class m220728_135239_foreignkeys_for_above_FIVE_migration_files extends Migratio
      */
     public function safeUp()
     {
-
+        $query = <<<EOF
+ALTER TABLE `studying_branch_name` ADD FOREIGN KEY (`field_id`) REFERENCES `studying_field_name`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `studying_branch_name` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `studying_field_name` ADD FOREIGN KEY (`university_id`) REFERENCES `studying_university_name`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `studying_field_name` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `studying_type` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `studying_university_name` ADD FOREIGN KEY (`type_id`) REFERENCES `studying_type`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `studying_university_name` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `user_current_education` ADD FOREIGN KEY (`education_type_id`) REFERENCES `studying_type`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `user_current_education` ADD FOREIGN KEY (`university_id`) REFERENCES `studying_university_name`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `user_current_education` ADD FOREIGN KEY (`studying_field_id`) REFERENCES `studying_field_name`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `user_current_education` ADD FOREIGN KEY (`studying_branch_id`) REFERENCES `studying_branch_name`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `user_current_education` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+EOF;
+        $this->execute($query);
     }
 
     /**
