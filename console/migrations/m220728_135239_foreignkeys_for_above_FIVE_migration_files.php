161391f82f835e4fa3602366931502e397e92175
DevParmar2101 <devp2101@gmail.com>
2022-07-28 19:23:40 +0530

    new migration file for foreign_keys of above five migration file table is added in this files

---
+++ console/migrations/m220728_135239_foreignkeys_for_above_FIVE_migration_files.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220728_135239_foreignkeys_for_above_FIVE_migration_files
+ */
+class m220728_135239_foreignkeys_for_above_FIVE_migration_files extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220728_135239_foreignkeys_for_above_FIVE_migration_files cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220728_135239_foreignkeys_for_above_FIVE_migration_files cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
