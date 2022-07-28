920131eb983ea115b8260b940919751c00a96162
DevParmar2101 <devp2101@gmail.com>
2022-07-28 19:20:34 +0530

    query is added in this user_current_education migration file

--- console/migrations/m220728_134447_create_user_current_education_table.php
+++ console/migrations/m220728_134447_create_user_current_education_table.php
@@ -14,7 +14,13 @@ class m220728_134447_create_user_current_education_table extends Migration
     {
         $this->createTable('{{%user_current_education}}', [
             'id' => $this->primaryKey(),
-            'education_type' => $th
+            'education_type_id' => $this->integer(11),
+            'university_id' => $this->integer(11),
+            'studying_field_id' => $this->integer(11)->defaultValue(null),
+            'studying_branch_id' => $this->integer(11)->defaultValue(null),
+            'last_year_percentage' => $this->string(11),
+            'user_id' => $this->integer(11),
+            'created_at' => $this->timestamp()
         ]);
     }
 
