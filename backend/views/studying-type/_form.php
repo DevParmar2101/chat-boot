6bc93a61dad665b7f333ebd2f502dd19d93c658f
Dev <devp2101@gmail.com>
2022-07-29 18:34:27 +0530

    fixes

--- backend/views/studying-type/_form.php
+++ backend/views/studying-type/_form.php
@@ -1,7 +1,8 @@
 <?php
 
+use kartik\select2\Select2;
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap4\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\StudyingType */
@@ -12,13 +13,15 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'studying_type_name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'user_id')->textInput() ?>
+    <?= $form->field($model, 'studying_type_name')->widget(Select2::class,[
+            'data' => $model->status(),
+            'options' => [
+                    'placeholder' => 'Select Status',
+                    'autoClear' => true
+            ]
+    ]) ?>
 
     <?= $form->field($model, 'status')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
