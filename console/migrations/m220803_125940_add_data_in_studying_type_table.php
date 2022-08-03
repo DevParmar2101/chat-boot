df003eeba5ebab13b809adedb37a44c08743756f
Dev <devp2101@gmail.com>
2022-08-03 18:52:44 +0530

    query is added in this migration files

--- console/migrations/m220803_125940_add_data_in_studying_type_table.php
+++ console/migrations/m220803_125940_add_data_in_studying_type_table.php
@@ -14,6 +14,17 @@ class m220803_125940_add_data_in_studying_type_table extends Migration
     {
         $query =<<<EOF
 
+INSERT INTO `studying_type` (`id`, `studying_type_name`, `user_id`, `status`, `created_at`) VALUES
+(1, '10th Class', 1, 10, '2022-07-29 17:48:18'),
+(2, '11th Class', 1, 10, '2022-07-29 17:54:11'),
+(3, '12th CLass', 1, 10, '2022-07-29 17:54:19'),
+(4, 'Undergraduate', 1, 10, '2022-07-29 17:55:42'),
+(5, 'Postgraduate', 1, 10, '2022-07-29 17:55:57'),
+(6, 'Doing PHD', 1, 10, '2022-07-29 17:56:18'),
+(7, 'Diploma', 1, 10, '2022-07-29 17:56:28'),
+(8, 'ITI (Industrial Training Institute)', 1, 10, '2022-07-29 17:57:26'),
+(9, 'Others', 1, 10, '2022-07-29 17:57:37');
+
 EOF;
         $this->execute($query);
     }
