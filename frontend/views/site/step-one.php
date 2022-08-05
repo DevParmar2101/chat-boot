216680ec9eab57e3fca0777a5e744639cc02d42e
DevParmar2101 <devp2101@gmail.com>
2022-08-05 17:34:29 +0530

    fixes

--- frontend/views/site/step-one.php
+++ frontend/views/site/step-one.php
@@ -1,17 +1,19 @@
 <?php
 
+use common\models\UserCurrentEducation;
+use yii\bootstrap4\ActiveForm;
+use yii\helpers\Html;
+use yii\web\View;
+
 /** @var $this View*/
 /** @var $form ActiveForm*/
 /** @var $user_education UserCurrentEducation*/
-
-use common\models\UserCurrentEducation;
-use yii\bootstrap4\ActiveForm;
-use yii\web\View;
-
+/** @var $form_information*/
 ?>
+
 <?php $form = ActiveForm::begin(['id'=>'step-one-form'])?>
 <div class="card-body">
-    <p>Please give same detail as in your college.</p>
+    <p><?= $form_information?></p>
     <div class="row">
         <div class="col-sm-6">
             <?= $form->field($user_education,'first_name')->textInput()?>
@@ -27,6 +29,6 @@ use yii\web\View;
     </div>
 </div>
 <div class="card-footer">
-    <?= \yii\helpers\Html::submitButton('Save & Next',['class' => 'btn btn-primary'])?>
+    <?= Html::submitButton('Save & Next',['class' => 'btn btn-primary'])?>
 </div>
 <?php ActiveForm::end()?>
