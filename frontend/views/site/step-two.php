a859c92cfd7232e940d75938bee75458bb6b3e8b
DevParmar2101 <devp2101@gmail.com>
2022-08-05 18:29:13 +0530

    pjax push-state is added in this view file

--- frontend/views/site/step-two.php
+++ frontend/views/site/step-two.php
@@ -1,3 +1,37 @@
 <?php
-echo 'Yes! I am Rendering';
+
+use common\components\ActiveForm;
+use common\models\UserCurrentEducation;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\web\View;
+
+/** @var $this View*/
+/** @var $form ActiveForm*/
+/** @var $user_education UserCurrentEducation*/
+/** @var $form_information*/
 ?>
+
+
+<?php $form = ActiveForm::begin(['id'=>'step-one-form'])?>
+    <div class="card-body">
+        <p><?= $form_information?></p>
+        <div class="row">
+            <div class="col-sm-6">
+                <?= $form->field($user_education,'first_name')->textInput()?>
+            </div>
+            <div class="col-sm-6">
+                <?= $form->field($user_education,'last_name')->textInput()?>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer">
+        <?= Html::submitButton('Save & Next',['class' => 'btn btn-primary'])?>
+    </div>
+<?php ActiveForm::end()?>
+<?php
+$current_page_url = Url::toRoute(['site/step-two']);
+$js_page_reload = <<<JS
+window.history.pushState('', '', "$current_page_url");
+JS;
+$this->registerJs($js_page_reload);
