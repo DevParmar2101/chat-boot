c77a30714614434e8c106429410cbb658151b0b2
DevParmar2101 <devp2101@gmail.com>
2022-08-05 14:56:51 +0530

    new data is added in this files

---
+++ frontend/views/site/step-one.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var $this View*/
+/** @var $form ActiveForm*/
+/** @var $model UserCurrentEducation*/
+
+use common\models\UserCurrentEducation;
+use yii\bootstrap4\ActiveForm;
+use yii\web\View;
+
+?>
+
+<div class="card">
+    <div class="card-header">
+        <h4>Your Current Education</h4>
+        <span class="text-sm-left text-secondary">Step 1/4</span>
+    </div>
+    <div class="card-body">
+
+    </div>
+</div>
