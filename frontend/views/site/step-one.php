e78fa402feb837c87f01a56ce48df2d093b69cd9
DevParmar2101 <devp2101@gmail.com>
2022-08-05 16:56:55 +0530

    variable enhancement

--- frontend/views/site/step-one.php
+++ frontend/views/site/step-one.php
@@ -2,38 +2,31 @@
 
 /** @var $this View*/
 /** @var $form ActiveForm*/
-/** @var $model UserCurrentEducation*/
+/** @var $user_education UserCurrentEducation*/
 
 use common\models\UserCurrentEducation;
 use yii\bootstrap4\ActiveForm;
 use yii\web\View;
 
 ?>
-
-<div class="card text-center mx-auto" style="display:block; width: 45rem">
-    <div class="card-header">
-        <h4>Your Current Education</h4>
-        <span class="text-sm-left text-secondary">Step 1/4</span>
-    </div>
-    <?php $form = ActiveForm::begin(['id'=>'step-one-form'])?>
-    <div class="card-body">
-        <p>Please give same detail as in your college.</p>
-        <div class="row">
-            <div class="col-sm-6">
-                <?= $form->field($model,'first_name')->textInput()?>
-            </div>
-            <div class="col-sm-6">
-                <?= $form->field($model,'last_name')->textInput()?>
-            </div>
+<?php $form = ActiveForm::begin(['id'=>'step-one-form'])?>
+<div class="card-body">
+    <p>Please give same detail as in your college.</p>
+    <div class="row">
+        <div class="col-sm-6">
+            <?= $form->field($user_education,'first_name')->textInput()?>
         </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <?= $form->field($model,'mobile_number')->textInput()?>
-            </div>
+        <div class="col-sm-6">
+            <?= $form->field($user_education,'last_name')->textInput()?>
         </div>
     </div>
-    <div class="card-footer">
-        <?= \yii\helpers\Html::submitButton('Save & Next',['class' => 'btn btn-primary'])?>
+    <div class="row">
+        <div class="col-sm-6">
+            <?= $form->field($user_education,'mobile_number')->textInput()?>
+        </div>
     </div>
-    <?php ActiveForm::end()?>
 </div>
+<div class="card-footer">
+    <?= \yii\helpers\Html::submitButton('Save & Next',['class' => 'btn btn-primary'])?>
+</div>
+<?php ActiveForm::end()?>
