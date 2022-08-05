0da7e53ce585e85f4965c1a0719c7265579218fa
DevParmar2101 <devp2101@gmail.com>
2022-08-05 18:28:43 +0530

    js bug fixes

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -12,112 +12,112 @@ use yii\helpers\Url;
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>" class="h-100">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body class="d-flex flex-column h-100 mt-10">
+    <?php $this->beginBody() ?>
 
-<header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="<?= Url::toRoute('/site/index')?>"><?= Yii::$app->name?></a>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="<?= Url::toRoute('/site/index')?>"><?= Yii::$app->name?></a>
 
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            </ul>
-            <?php if (!Yii::$app->user->isGuest){?>
-            <ul class="navbar-nav my-2 my-lg-0">
-                <ul class="nav justify-content-end">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="<?= Url::toRoute('/site/index')?>">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="<?= Url::toRoute('/site/about')?>">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="<?= Url::toRoute('/site/contact')?>">Contact</a>
-                    </li>
-                </ul>
-                <div class="dropdown">
-                    <a class="nav-link dropdown-toggle profile-header-dropdown" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <?= Yii::$app->user->identity->username?>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="<?= Url::toRoute('site/profile')?>">Profile</a>
-                        <?= Html::a('Logout', Url::to(['site/logout']),['data-method' => 'POST','class' => 'dropdown-item'])?>
-                    </div>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    </ul>
+                    <?php if (!Yii::$app->user->isGuest){?>
+                        <ul class="navbar-nav my-2 my-lg-0">
+                            <ul class="nav justify-content-end">
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="<?= Url::toRoute('/site/index')?>">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="<?= Url::toRoute('/site/about')?>">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="<?= Url::toRoute('/site/contact')?>">Contact</a>
+                                </li>
+                            </ul>
+                            <div class="dropdown">
+                                <a class="nav-link dropdown-toggle profile-header-dropdown" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <?= Yii::$app->user->identity->username?>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="<?= Url::toRoute('site/profile')?>">Profile</a>
+                                    <?= Html::a('Logout', Url::to(['site/logout']),['data-method' => 'POST','class' => 'dropdown-item'])?>
+                                </div>
+                            </div>
+                        </ul>
+                    <?php }else{?>
+                        <ul class="nav justify-content-end">
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="<?= Url::toRoute('/')?>">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="<?= Url::toRoute('/site/about')?>">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="<?= Url::toRoute('/site/contact')?>">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="<?= Url::toRoute('/site/signup')?>">Signup</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="<?= Url::toRoute('/site/login')?>">Login</a>
+                            </li>
+                        </ul>
+                    <?php }?>
                 </div>
-            </ul>
-            <?php }else{?>
-                <ul class="nav justify-content-end">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="<?= Url::toRoute('/')?>">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="<?= Url::toRoute('/site/about')?>">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="<?= Url::toRoute('/site/contact')?>">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="<?= Url::toRoute('/site/signup')?>">Signup</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="<?= Url::toRoute('/site/login')?>">Login</a>
-                    </li>
-                </ul>
-            <?php }?>
-        </div>
-        </div>
+            </div>
 
-    </nav>
-</header>
+        </nav>
+    </header>
 
-<main role="main" class="flex-shrink-0">
-    <div id="preloader" style="display: none;">
-        <div class="lds-ripple">
-            <div></div>
-            <div></div>
+    <main role="main" class="flex-shrink-0">
+        <div id="preloader" style="display: none;">
+            <div class="lds-ripple">
+                <div></div>
+                <div></div>
+            </div>
         </div>
-    </div>
-    <div id="preloader-pjax" style="display: none;">
-        <div class="lds-ripple">
-            <div></div>
-            <div></div>
+        <div id="preloader-pjax" style="display: none;">
+            <div class="lds-ripple">
+                <div></div>
+                <div></div>
+            </div>
         </div>
-    </div>
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <div class="content-body content-height">
-
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <div class="content-body content-height">
+                <?= $content ?>
+            </div>
         </div>
-        <?= $content ?>
-    </div>
-</main>
+    </main>
 
-<footer class="footer mt-auto py-3 text-muted">
-    <div class="container">
-        <p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+    <footer class="footer mt-auto py-3 text-muted">
+        <div class="container">
+            <p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+            <p class="float-right"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
 
-<?php $this->endBody() ?>
-<?php
-$js_pjax =<<<JS
+    <?php $this->endBody() ?>
+    <?php
+    $product_url = Url::toRoute(['site/add-multiple-distance']);
+    $js_pjax =<<<JS
 $.pjax.defaults.scrollTo=false;
 $("body").bind("ajaxComplete", function(e, xhr, settings){
     var handleMinHeight = function() {
@@ -159,9 +159,34 @@ $("body").on("submit", "form", function() {
     });
     return true;
 });
+
+
+    $(document).on('click',".btn-add-product",function(){  
+        var product = $(this).data('product');
+        var div = $(this).data('div');
+        var counter = 'counter-'+ div;
+        var value_counter = $('.'+counter).val();
+        var new_value_counter = 0;
+
+        if(!value_counter){
+            value_counter = 1;
+        }
+         new_value_counter = parseInt(value_counter)+1;
+        console.log(product);
+        console.log(div);
+        console.log(counter);
+        console.log(value_counter);
+        console.log(new_value_counter);
+        $('.'+counter).val(new_value_counter);
+        $.get("$product_url?id="+product+"&value="+value_counter, function(data, status){
+            $("#"+div).append(data);
+            $(".sub-product-title-"+product).removeClass('d-none');
+            $(".product-price-"+product).addClass('d-none');
+        });
+    });
 JS;
-$this->registerJs($js_pjax)
-?>
-</body>
-</html>
+    $this->registerJs($js_pjax)
+    ?>
+    </body>
+    </html>
 <?php $this->endPage();
