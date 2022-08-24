bc84e477ba8094165a6c558949e283a9da08b9f4
DevParmar2101 <devp2101@gmail.com>
2022-08-24 11:49:13 +0530

    new file for user-list is added in this file

---
+++ frontend/views/site/user-list.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+
+<div class="row">
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
